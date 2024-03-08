ed350c1e136a2c9614c9a09b471bce939f95894d
Zahari Petkov <zahari@balena.io>
2024-03-08 11:47:49 +0200

    Extract hash failure tests in separate module
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- test/all.spec.ts
+++ test/all.spec.ts
@@ -1,13 +1,12 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import { describe } from 'mocha';
-import * as stream from 'node:stream';
 
 import * as bundle from '../lib';
 
 import type { Resource } from '../lib/types';
 
-import { stringToStream, createTarBundle, streamToString } from './utils';
+import { stringToStream, streamToString } from './utils';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -100,65 +99,6 @@ describe('basic usage', () => {
 				},
 			],
 		]);
-	});
-
-	it('add resource with bad hash', async () => {
-		const writable = bundle.create({
-			type: 'foo@1',
-			manifest: ['hello.txt'],
-			resources: [
-				{
-					id: 'hello',
-					size: 5,
-					digest: 'sha256:deadbeef',
-				},
-			],
-		});
-
-		const hello = stringToStream('hello');
-
-		try {
-			await writable.addResource('hello', hello);
-			expect.fail('Unreachable');
-		} catch (error) {
-			expect(error.message).to.equal(
-				'Expected digest sha256:deadbeef does not match calculated digest sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-			);
-		}
-	});
-
-	it('read resource with bad hash', async () => {
-		const pack = createTarBundle({
-			version: '1',
-			type: 'foo@1',
-			manifest: ['hello.txt'],
-			resources: [
-				{
-					id: 'hello',
-					size: 5,
-					digest: 'sha256:deadbeef',
-				},
-			],
-		});
-
-		pack.entry({ name: 'resources/deadbeef' }, 'hello');
-
-		pack.finalize();
-
-		const readable = bundle.open(pack, 'foo@1');
-
-		await readable.manifest();
-
-		try {
-			for await (const { resource } of readable.resources()) {
-				await stream.promises.finished(resource);
-			}
-			expect.fail('Unreachable');
-		} catch (error) {
-			expect(error.message).to.equal(
-				'Expected digest sha256:deadbeef does not match calculated digest sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-			);
-		}
 	});
 
 	it('read resources without accessing manifest', async () => {
