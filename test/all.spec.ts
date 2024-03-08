401935c623ab8a3aa2705c3aafaf30b9d31e70f2
Zahari Petkov <zahari@balena.io>
2024-03-08 11:30:56 +0200

    Extract read/write resources failure tests in separate module
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- test/all.spec.ts
+++ test/all.spec.ts
@@ -4,14 +4,10 @@ import { describe } from 'mocha';
 import * as stream from 'node:stream';
 
 import * as bundle from '../lib';
+
 import type { Resource } from '../lib/types';
 
-import {
-	stringToStream,
-	ErroringStream,
-	createTarBundle,
-	streamToString,
-} from './utils';
+import { stringToStream, createTarBundle, streamToString } from './utils';
 
 chai.use(chaiAsPromised);
 const expect = chai.expect;
@@ -104,61 +100,6 @@ describe('basic usage', () => {
 				},
 			],
 		]);
-	});
-
-	it('add resource with stream throwing an error', async () => {
-		const myBundle = bundle.create({
-			type: 'foo@1',
-			manifest: ['hello.txt'],
-			resources: [
-				{
-					id: 'hello',
-					size: 5,
-					digest:
-						'sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-				},
-			],
-		});
-
-		const hello = new ErroringStream('hello');
-
-		try {
-			await myBundle.addResource('hello', hello);
-			expect.fail('Unreachable');
-		} catch (error) {
-			expect(error.message).to.equal('ErroringStream is throwing an error');
-		}
-	});
-
-	it('add resource with wrong size', async () => {
-		const myBundle = bundle.create({
-			type: 'foo@1',
-			manifest: ['hello.txt'],
-			resources: [
-				{
-					id: 'hello',
-					size: 100,
-					digest:
-						'sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
-				},
-			],
-		});
-
-		try {
-			const hello = stringToStream('hello');
-			await myBundle.addResource('hello', hello);
-			expect.fail('Unreachable');
-		} catch (error) {
-			// TODO: Can we add the resource ID in the error message?
-			expect(error.message).to.equal('Size mismatch');
-		}
-
-		try {
-			await myBundle.finalize();
-			expect.fail('Unreachable');
-		} catch (error) {
-			expect(error.message).to.equal('Size mismatch');
-		}
 	});
 
 	it('add resource with bad hash', async () => {
---
+++ test/resource.spec.ts
@@ -0,0 +1,67 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+import { describe } from 'mocha';
+
+import * as bundle from '../lib';
+
+import { ErroringStream, stringToStream } from './utils';
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe('read/write resources failures', () => {
+	it('add resource with stream throwing an error', async () => {
+		const myBundle = bundle.create({
+			type: 'foo@1',
+			manifest: ['hello.txt'],
+			resources: [
+				{
+					id: 'hello',
+					size: 5,
+					digest:
+						'sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+				},
+			],
+		});
+
+		const hello = new ErroringStream('hello');
+
+		try {
+			await myBundle.addResource('hello', hello);
+			expect.fail('Unreachable');
+		} catch (error) {
+			expect(error.message).to.equal('ErroringStream is throwing an error');
+		}
+	});
+
+	it('add resource with wrong size', async () => {
+		const myBundle = bundle.create({
+			type: 'foo@1',
+			manifest: ['hello.txt'],
+			resources: [
+				{
+					id: 'hello',
+					size: 100,
+					digest:
+						'sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+				},
+			],
+		});
+
+		try {
+			const hello = stringToStream('hello');
+			await myBundle.addResource('hello', hello);
+			expect.fail('Unreachable');
+		} catch (error) {
+			// TODO: Can we add the resource ID in the error message?
+			expect(error.message).to.equal('Size mismatch');
+		}
+
+		try {
+			await myBundle.finalize();
+			expect.fail('Unreachable');
+		} catch (error) {
+			expect(error.message).to.equal('Size mismatch');
+		}
+	});
+});
