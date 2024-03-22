b6e10488e31bf1b205b39ac4eb7e046e4ea8154c
Zahari Petkov <zahari@balena.io>
2024-03-22 08:57:51 +0200

    Revert 270789a which was committed to master by mistake
    Change-type: patch
    Signed-off-by: Zahari Petkov <zahari@balena.io>

--- src/writable.ts
+++ src/writable.ts
@@ -28,7 +28,6 @@ class WritableBundle<T> {
 	private packError: Error | undefined;
 	private lastResourcePromise: Promise<void> | undefined;
 	private addedChecksums: string[];
-	private addedResources: string[];
 
 	constructor(
 		type: string,
@@ -66,7 +65,6 @@ class WritableBundle<T> {
 		this.pack = pack;
 		this.resources = resources;
 		this.addedChecksums = [];
-		this.addedResources = [];
 	}
 
 	public async addResource(
@@ -118,8 +116,6 @@ class WritableBundle<T> {
 
 		await promise;
 
-		this.addedResources.push(id);
-
 		return true;
 	}
 
@@ -129,13 +125,6 @@ class WritableBundle<T> {
 		}
 
 		await this.lastResourcePromise;
-
-		for (const { id } of this.resources) {
-			const index = this.addedResources.indexOf(id);
-			if (index === -1) {
-				throw new Error(`Resource "${id}" was not added`);
-			}
-		}
 
 		this.pack.finalize();
 
